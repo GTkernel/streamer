af34ad8f92649816d85dc0f3aed520090871fd4b
Ran Xian <xranthoar@gmail.com>
2016-07-23 22:31:18 -0700

    Add display as a option to tx1_run_alexnet

--- apps/tx1_run_alexnet.cpp
+++ apps/tx1_run_alexnet.cpp
@@ -4,33 +4,45 @@
 int
 main (int argc, char *argv[])
 {
-  if (argc != 6) {
+  if (argc != 6 && argc != 7) {
     std::cout << "Usage: " << argv[0]
               << " deploy.prototxt network.caffemodel"
-              << " mean.binaryproto labels.txt RSTPURI"<< std::endl;
+              << " mean.binaryproto labels.txt RTSPURI"
+              << " display" << std::endl;
+    std::cout << "";
+    std::cout << "  RTSPURI: uri to the camera. e.g rtsp://xxx" << std::endl;
+    std::cout << "  display: enable display or not, must have a X window" << std::endl;
     exit(1);
   }
 
-  // glog
+  // Set up
   gst_init(&argc, &argv);
   google::InitGoogleLogging(argv[0]);
   FLAGS_alsologtostderr = 1;
   FLAGS_colorlogtostderr = 1;
   FLAGS_minloglevel = 0;
 
+  // Get options
   string model_file   = argv[1];
   string trained_file = argv[2];
   string mean_file    = argv[3];
   string label_file   = argv[4];
-  CaffeClassifier classifier(model_file, trained_file, mean_file, label_file);
+  bool display = false;
+  if (argc == 7) {
+    display = true;
+  }
 
+  CaffeClassifier classifier(model_file, trained_file, mean_file, label_file);
   GstVideoCapture cap;
   if (!cap.CreatePipeline(argv[5])) {
     LOG(FATAL) << "Can't create pipeline, check camera and pipeline uri";
     exit(1);
   }
 
-//  cv::namedWindow("camera");
+  if (display) {
+    cv::namedWindow("camera");
+  }
+
   while(1) {
     cv::Mat frame = cap.GetFrame();
 
@@ -39,8 +51,10 @@ main (int argc, char *argv[])
       CaffeClassifier::Prediction p = predictions[0];
       LOG(INFO) << std::fixed << std::setprecision(4) << p.second << " - \""
                 << p.first << "\"" << std::endl;
-//      cv::imshow("camera", frame);
-//      cv::waitKey(30);
+      if (display) {
+      cv::imshow("camera", frame);
+      cv::waitKey(30);
+      }
     } else {
       LOG(INFO) << "Got empty frame";
       break;
