a811f046bdcd3586390830b6b5def0a7feb2dea7
Tony Chen <xiaolongx.chen@intel.com>
2017-06-02 16:26:01 +0800

    tracker_struck display sheared uuid

--- apps/tracker_struck/tracker_struck.cpp
+++ apps/tracker_struck/tracker_struck.cpp
@@ -192,12 +192,18 @@ void Run(const std::vector<string> &camera_names,
         }
         auto tags = md_frame->GetTags();
         auto confidences = md_frame->GetConfidences();
+        auto uuids = md_frame->GetUuids();
         for (size_t j = 0; j < tags.size(); ++j) {
           std::ostringstream text;
+          text << tags[j];
           if (tags.size() == confidences.size())
-            text << tags[j] << "  :  " << confidences[j];
-          else
-            text << tags[j];
+            text << "  :  " << confidences[j];
+          if (tags.size() == uuids.size()) {
+            std::size_t pos = uuids[j].size();
+            auto sheared_uuid = uuids[j].substr(pos-5);
+            text << "  :  " << sheared_uuid;
+          }
+          
           cv::putText(image, text.str() , cv::Point(bboxes[j].px,bboxes[j].py+30) , 0 , 1.0 , cv::Scalar(0,255,0), 3 );
         }  
         cv::imshow(camera_names[i], image);
