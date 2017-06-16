c58802a9f5cfeb63de6c333a4b950cac9cff48a1
Tony Chen <xiaolongx.chen@intel.com>
2017-06-16 17:14:48 +0800

    Add CV utils

---
+++ src/utils/cv_utils.h
@@ -0,0 +1,60 @@
+/**
+* CV utils
+* 
+* @author Tony Chen <xiaolongx.chen@intel.com>
+* @author Shao-Wen Yang <shao-wen.yang@intel.com>
+*/
+
+#ifndef STREAMER_CV_UTILS_H_H
+#define STREAMER_CV_UTILS_H_H
+#include "common/common.h"
+
+inline cv::Scalar HSV2RGB(const float h, const float s, const float v) {
+  const int h_i = static_cast<int>(h * 6);
+  const float f = h * 6 - h_i;
+  const float p = v * (1 - s);
+  const float q = v * (1 - f*s);
+  const float t = v * (1 - (1 - f) * s);
+  float r, g, b;
+  switch (h_i) {
+    case 0:
+      r = v; g = t; b = p;
+      break;
+    case 1:
+      r = q; g = v; b = p;
+      break;
+    case 2:
+      r = p; g = v; b = t;
+      break;
+    case 3:
+      r = p; g = q; b = v;
+      break;
+    case 4:
+      r = t; g = p; b = v;
+      break;
+    case 5:
+      r = v; g = p; b = q;
+      break;
+    default:
+      r = 1; g = 1; b = 1;
+      break;
+  }
+  return cv::Scalar(r * 255, g * 255, b * 255);
+}
+
+// http://martin.ankerl.com/2009/12/09/how-to-create-random-colors-programmatically
+inline std::vector<cv::Scalar> GetColors(const int n) {
+  std::vector<cv::Scalar> colors;
+  cv::RNG rng(12345);
+  const float golden_ratio_conjugate = 0.618033988749895;
+  const float s = 0.3;
+  const float v = 0.99;
+  for (int i = 0; i < n; ++i) {
+    const float h = std::fmod(rng.uniform(0.f, 1.f) + golden_ratio_conjugate,
+                              1.f);
+    colors.push_back(HSV2RGB(h, s, v));
+  }
+  return colors;
+}
+
+#endif  // STREAMER_CV_UTILS_H_H
