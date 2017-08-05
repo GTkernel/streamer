008cc2fbc3c852ece2fb05ba5fb19ef49d3e0058
Shao-Wen Yang <shao-wen.yang@intel.com>
2017-08-04 22:59:34 -0700

    Added NCS source

---
+++ src/ncs/ncs.h
@@ -0,0 +1,6 @@
+#ifndef __NCS_API__
+#define __NCS_API__
+
+#include "ncs_manager.h"
+
+#endif // __NCS_API__
---
+++ src/ncs/ncs_manager.h
@@ -0,0 +1,74 @@
+/**
+ * An interface to Neural Compute Stick
+ *
+ * @author Shao-Wen Yang <shao-wen.yang@intel.com>
+ *
+ */
+
+#ifndef __NCS_MANAGER_H__
+#define __NCS_MANAGER_H__
+
+#include <string>
+#include <vector>
+
+#include "opencv2/opencv.hpp"
+
+#include <atomic>
+#include <thread>
+#include "boost/lockfree/spsc_queue.hpp"
+
+class NCSManager {
+private:
+    const std::string _model_path;
+    const cv::Size _image_size;
+    const unsigned int _input_size;
+    const float _mean[3];
+    const float _std[3];
+
+    std::atomic<bool> _done;
+    std::thread* _it;
+    boost::lockfree::spsc_queue<std::pair<int, void*>, boost::lockfree::fixed_sized<false>> _iq;
+
+    // TODO: SPSC queue for output as well?
+
+    std::vector<std::string> _names;
+    std::vector<void*> _devices;
+    std::vector<void*> _graphs;
+    
+    void Start();
+    void Stop();
+    
+    static void* LoadGraph(const char* path, unsigned int* length);
+
+public:
+    NCSManager(const char* model_path, int dim);
+
+    int Open();
+
+    void LoadImage(const char* filename);
+    void LoadImage(const cv::Mat& image);
+    void GetResult(std::vector<float>& result);
+    
+    void LoadImage(int i, const char* filename);
+    void LoadImage(int i, const cv::Mat& image);
+    void GetResult(int, std::vector<float>& result);
+
+    void Close();
+    
+    bool IsOpened() const;
+    int GetNumDevices() const;
+    
+    static int EnumerateDevices(std::vector<std::string>& names);
+    
+    static void* OpenDevice(const std::string& name);
+    static void CloseDevice(void* handle);
+
+    static void* AllocateGraph(void* handle, const char* path);
+    static void DeallocateGraph(void* handle);
+ 
+
+    ~NCSManager();
+
+};
+
+#endif // __NCS_MANAGER_H__
