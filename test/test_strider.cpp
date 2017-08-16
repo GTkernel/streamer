7d0d741221d2bae87a65ef75b1449f1fa9be9a4d
Michael Kaminsky <kaminsky@cs.cmu.edu>
2017-08-16 17:13:28 -0400

    Rewrite test without gstreamer

--- test/test_strider.cpp
+++ test/test_strider.cpp
@@ -1,29 +1,30 @@
 #include <gtest/gtest.h>
-#include "camera/gst_camera.h"
+#include "stream/stream.h"
 #include "processor/strider.h"
 
 TEST(StriderTest, TestBasic) {
   auto stride = 10;
-  auto camera_name = "TEST_CAMERA";
-  auto video_uri = "gst://videotestsrc ! video/x-raw,width=640,height=480";
-  GSTCamera camera(camera_name, video_uri, 640, 480);
+  auto stream = std::make_shared<Stream>();
 
   Strider strider(stride);
-  strider.SetSource(camera.GetStream());
-
+  strider.SetSource(stream);
   auto reader = strider.GetSink("output")->Subscribe();
-
   strider.Start();
-  camera.Start();
 
-  auto current = 0;
+  for (unsigned long i = 0; i < 50; i++) {
+    auto frame = std::make_unique<Frame>();
+    frame->SetValue("frame_id", i);
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    stream->PushFrame(std::move(frame));
+  }
+
   for (auto i = 0; i < 5; i++) {
+    auto current = i * stride;
+    LOG(INFO) << "Waiting for frame_id = " << current;
     auto id = reader->PopFrame()->GetValue<unsigned long>("frame_id");
-    EXPECT_EQ(id, current);
-    current += stride;
+    ASSERT_EQ(id, current);
   }
 
   reader->UnSubscribe();
-  camera.Stop();
   strider.Stop();
 }
