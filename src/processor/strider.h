c1d6b2794deb2ef8abd8fa7e33e454e7e414a61f
Angela Jiang <to.ajiang@gmail.com>
2017-08-16 13:04:50 -0400

    Another comment change

--- src/processor/strider.h
+++ src/processor/strider.h
@@ -8,14 +8,9 @@
 #include "common/types.h"
 #include "processor/processor.h"
 
-// Strider performs admission control of frames to limit the number
-// of outstanding frames in the pipeline. It should be used together with
-// FlowControlExit.
+// Strider passes through frames at a user-specific stride
 class Strider : public Processor {
  public:
-  // "max_tokens" should not be larger than the capacity of the shortest stream
-  // queue in the flow control domain. This is to ensure that no frames are
-  // dropped due to queue overflow.
   Strider(unsigned int stride);
   static std::shared_ptr<Strider> Create(const FactoryParamsType& params);
 
@@ -31,8 +26,6 @@ class Strider : public Processor {
   virtual void Process() override;
 
  private:
-  // Used to verify that num_tokens_available_ never exceeds the original number
-  // of tokens.
   unsigned int stride_;
   unsigned int num_frames_processed_ = 0;
 };
