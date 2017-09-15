9d4956065c74974905feeb60abecc5b60cd53035
Christopher Canel <c.canel@icloud.com>
2017-09-15 18:34:13 -0400

    Clean up test_gst_video_encoder.cpp

--- test/test_gst_video_encoder.cpp
+++ test/test_gst_video_encoder.cpp
@@ -1,41 +1,43 @@
-//
-// Created by Ran Xian (xranthoar@gmail.com) on 10/26/16.
-//
+
+#include <memory>
 
 #include <gtest/gtest.h>
-#include <streamer.h>
 
-TEST(GST_VIDEO_ENCODER_TEST, FILE_TEST) {
-  CameraManager& camera_manager = CameraManager::GetInstance();
+#include "camera/camera_manager.h"
+#include "stream/frame.h"
+#include "utils/utils.h"
+#include "video/gst_video_encoder.h"
 
-  auto camera = camera_manager.GetCamera("GST_TEST");
-  GstVideoEncoder encoder(camera->GetWidth(), camera->GetHeight(), "test.mp4");
-  encoder.SetSource("input", camera->GetStream());
+TEST(TestGstVideoEncoder, TestFile) {
+  auto camera = CameraManager::GetInstance().GetCamera("GST_TEST");
+  auto encoder = std::make_shared<GstVideoEncoder>(
+      camera->GetWidth(), camera->GetHeight(), "test.mp4");
+  encoder->SetSource("input", camera->GetStream());
+
+  auto encoder_reader = encoder->GetSink("output")->Subscribe();
   camera->Start();
-  encoder.Start();
+  encoder->Start();
 
-  STREAMER_SLEEP(100);
-
-  auto encoder_reader = encoder.GetSink("output")->Subscribe();
   auto image_frame = encoder_reader->PopFrame();
 
-  encoder.Stop();
+  encoder_reader->UnSubscribe();
+  encoder->Stop();
   camera->Stop();
 
   std::remove("test.mp4");
 }
 
-TEST(GST_VIDEO_ENCODER_TEST, STREAM_TEST) {
-  CameraManager& camera_manager = CameraManager::GetInstance();
+TEST(TestGstVideoEncoder, TestStream) {
+  auto camera = CameraManager::GetInstance().GetCamera("GST_TEST");
+  auto encoder = std::make_shared<GstVideoEncoder>(camera->GetWidth(),
+                                                   camera->GetHeight(), 12345);
+  encoder->SetSource("input", camera->GetStream());
 
-  auto camera = camera_manager.GetCamera("GST_TEST");
-  GstVideoEncoder encoder(camera->GetWidth(), camera->GetHeight(), 12345);
-  encoder.SetSource("input", camera->GetStream());
   camera->Start();
-  encoder.Start();
+  encoder->Start();
 
   STREAMER_SLEEP(100);
 
-  encoder.Stop();
+  encoder->Stop();
   camera->Stop();
 }
