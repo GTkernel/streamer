6c9510ed6055573ac67e365bf6efb764eaa01345
Christopher Canel <c.canel@icloud.com>
2017-10-30 21:59:39 -0400

    Clean up Display processor

--- src/processor/display.cpp
+++ src/processor/display.cpp
@@ -1,30 +1,55 @@
 
 #include "processor/display.h"
-#include "utils/image_utils.h"
 
 #include <cstdio>
+#include <sstream>
+#include <stdexcept>
+
+#include <opencv2/opencv.hpp>
+
+#include "stream/frame.h"
+#include "utils/image_utils.h"
 
 constexpr auto SOURCE_NAME = "input";
 constexpr auto SINK_NAME = "output";
 
-Display::Display(std::string key, unsigned int angle, float zoom,
-                 std::string window_name)
+Display::Display(const std::string& key, unsigned int angle, float size_ratio,
+                 const std::string& window_name)
     : Processor(PROCESSOR_TYPE_DISPLAY, {SOURCE_NAME}, {SINK_NAME}),
       key_(key),
       angle_(angle),
-      zoom_(zoom),
+      size_ratio_(size_ratio),
       window_name_(window_name) {
-  CHECK(zoom >= 0 && zoom <= 1) << "Display zoom must be between 0 and 1";
-  CHECK(angle == 0 || angle == 90 || angle == 180 || angle == 270)
-      << "Display angle must be one of {0, 90, 180, 270}";
+  std::unordered_set<unsigned int> possible_angles = {0, 90, 180, 270};
+  if (possible_angles.find(angle_) == possible_angles.end()) {
+    std::ostringstream msg;
+    msg << "\"angle\" must be one of { ";
+    for (auto possible_angle : possible_angles) {
+      msg << possible_angle << " ";
+    }
+    msg << "}, but is: " << angle_;
+    throw std::invalid_argument(msg.str());
+  }
+  if (size_ratio_ < 0 || size_ratio_ > 1) {
+    std::ostringstream msg;
+    msg << "\"size_ratio\" must be in the range [0, 1], but is: "
+        << size_ratio_;
+    throw std::invalid_argument(msg.str());
+  }
 }
 
 std::shared_ptr<Display> Display::Create(const FactoryParamsType& params) {
   std::string key = params.at("key");
-  unsigned int angle = std::stoi(params.at("angle"));
-  double zoom = std::stod(params.at("zoom"));
+  int angle = std::stoi(params.at("angle"));
+  if (angle < 0) {
+    std::ostringstream msg;
+    msg << "\"angle\" cannot be negative, but is: " << angle;
+    throw std::invalid_argument(msg.str());
+  }
+  double size_ratio = std::stod(params.at("size_ratio"));
   std::string window_name = params.at("window_name");
-  return std::make_shared<Display>(key, angle, zoom, window_name);
+  return std::make_shared<Display>(key, (unsigned int)angle, size_ratio,
+                                   window_name);
 }
 
 void Display::SetSource(StreamPtr stream) {
@@ -38,17 +63,19 @@ bool Display::Init() { return true; }
 bool Display::OnStop() { return true; }
 
 void Display::Process() {
-  auto frame = GetFrame(SOURCE_NAME);
+  std::unique_ptr<Frame> frame = GetFrame(SOURCE_NAME);
   const cv::Mat& img = frame->GetValue<cv::Mat>(key_);
-  cv::Mat m;
-  if (zoom_ >= 0)
-    cv::resize(img, m, cv::Size(), zoom_, zoom_);
-  else
-    cv::resize(img, m, cv::Size(), 1, 1);
-  RotateImage(m, angle_);
-  cv::imshow(window_name_, m);
 
-  unsigned char q = cv::waitKey(10);
+  cv::Mat display_img;
+  if (size_ratio_ >= 0) {
+    cv::resize(img, display_img, cv::Size(), size_ratio_, size_ratio_);
+  } else {
+    display_img = img;
+  }
+  RotateImage(display_img, angle_);
+
+  cv::imshow(window_name_, display_img);
+  cv::waitKey(10);
 
   PushFrame(SINK_NAME, std::move(frame));
 }
--- src/processor/display.h
+++ src/processor/display.h
@@ -1,16 +1,23 @@
+
 #ifndef STREAMER_PROCESSOR_DISPLAY_H_
 #define STREAMER_PROCESSOR_DISPLAY_H_
 
 #include <memory>
+#include <string>
 
 #include "common/types.h"
 #include "processor/processor.h"
 
-// The Display processor displays frames at a specified zoom and angle
+// Displays frames in an OpenCV window at a specified size ratio and rotation
+// angle. After being displayed, frames are passed unchanged to the next
+// Processor.
 class Display : public Processor {
  public:
-  Display(std::string key, unsigned int angle, float zoom,
-          std::string window_name_);
+  // "key" is the frame key to display, which must correspond to a cv::Mat
+  // object. "window_name" is the name to give the display window, which must be
+  // unique.
+  Display(const std::string& key, unsigned int angle, float size_ratio,
+          const std::string& window_name);
   static std::shared_ptr<Display> Create(const FactoryParamsType& params);
 
   void SetSource(StreamPtr stream);
@@ -27,7 +34,7 @@ class Display : public Processor {
  private:
   std::string key_;
   unsigned int angle_;
-  float zoom_;
+  float size_ratio_;
   std::string window_name_;
 };
 
