cb1aaad559ff4c6eeb348a67db2656a4f627b19a
Thomas Kim <thomas.kim@cs.cmu.edu>
2018-03-06 14:26:09 -0500

    Revert prev fix for CV_32F in jpeg_writer

--- src/processor/jpeg_writer.cpp
+++ src/processor/jpeg_writer.cpp
@@ -63,15 +63,7 @@ void JpegWriter::Process() {
            << GetDateTimeString(capture_time_micros) << "_" << field_ << ".jpg";
   std::string filepath_s = filepath.str();
   try {
-    // OpenCV expects float images to be normalized between 0 and 1,
-    // and 8-bit pixel format to be 0-255.
-    cv::Mat img_normalized;
-    if (img.type() != CV_8UC3) {
-      cv::normalize(img, img_normalized, 0, 1, cv::NORM_MINMAX);
-    } else {
-      img_normalized = img;
-    }
-    cv::imwrite(filepath_s, img_normalized);
+    cv::imwrite(filepath_s, img);
   } catch (cv::Exception& e) {
     LOG(FATAL) << "Unable to write JPEG file \"" << filepath_s
                << "\": " << e.what();
