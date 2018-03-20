9d3514c91bfe591476d47d116928685006f0f613
Christopher Canel <c.canel@icloud.com>
2018-03-20 13:42:46 -0400

    Cleanup to make most of the apps run

--- apps/save_vishashes/save_vishashes.cpp
+++ apps/save_vishashes/save_vishashes.cpp
@@ -78,17 +78,17 @@ void Run(bool block, const std::string& camera_name,
   nne->SetSource(transformer->GetSink());
   nne->SetBlockOnPush(block);
   procs.push_back(nne);
-  StreamPtr nne_stream = nne->GetSink(layer);
+  StreamPtr nne_stream = nne->GetSink();
 
   // Create FrameWriter.
   auto frame_writer = std::make_shared<FrameWriter>(
-      std::unordered_set<std::string>{"frame_id", "activations"}, output_dir,
+      std::unordered_set<std::string>{"frame_id", layer}, output_dir,
       FrameWriter::FileFormat::JSON, false, false, frames_per_dir);
   frame_writer->SetSource(nne_stream);
   frame_writer->SetBlockOnPush(block);
   procs.push_back(frame_writer);
 
-  auto reader = nne->GetSink(layer)->Subscribe();
+  auto reader = nne->GetSink()->Subscribe();
 
   std::thread progress_thread =
       std::thread([nne_stream] { ProgressTracker(nne_stream); });
