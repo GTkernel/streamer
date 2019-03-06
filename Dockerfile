d7e64bfb9f8a97eb0baa9fab26f7f014108f4c0d
Chameleon Cloud User <cc@tess1.novalocal>
2019-03-06 03:51:57 +0000

    cuda 9 prepare

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,8 @@
 #   Author: Ke-Jou Hsu <nosus_hsu@gatech.edu>
 
 ARG DEFAULT_WORKDIR=/vcs
-FROM streamer_env:tf_v0.5
+#FROM streamer_env:tf_v0.5
+FROM streamer_env:1-5-1-devel-gpu
 
 COPY ./ $DEFAULT_WORKDIR/streamer
 
@@ -11,10 +12,11 @@ WORKDIR $DEFAULT_WORKDIR/streamer
 
 ENV LD_LIBRARY_PATH ${LD_LIBRARY_PATH}:/usr/local/include
 
-RUN mkdir build && \ 
-    cd build && \
-    cmake -DCMAKE_BUILD_TYPE=Release -DBACKEND=cpu -DUSE_TENSORFLOW=yes -DTENSORFLOW_HOME=/usr/local/include/tensorflow/ .. && \
-    make -j8 && make apps -j8
+RUN mkdir build 
+#&& \ 
+#    cd build && \
+#    cmake -DCMAKE_BUILD_TYPE=Release -DBACKEND=cuda -DUSE_TENSORFLOW=yes -DTENSORFLOW_HOME=/usr/local/include/tensorflow/ .. && \
+#    make -j8 && make apps -j8
 
-WORKDIR $DEFAULT_WORKDIR/streamer/build
+#WORKDIR $DEFAULT_WORKDIR/streamer/build
 
--- docker/tf_env/script/tensorflow_setup.sh
+++ docker/tf_env/script/tensorflow_setup.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 
-BAZEL_VERSION=0.8.0
-#downgrade bazel
-curl -LO "https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/bazel_${BAZEL_VERSION}-linux-x86_64.deb" 
-dpkg -i bazel_*.deb
-
-#install tensorflow
-cd /vcs
-git clone https://github.com/tensorflow/tensorflow.git
-
-cd ./tensorflow
-git fetch
-git checkout r1.5
-echo -ne '\n\n\n\n\n\n\n\n\n\n\n\n\n\n' | ./configure
-bazel build --config=monolithic --config=opt //tensorflow:libtensorflow_cc.so
-bazel build --config=opt //tensorflow/tools/pip_package:build_pip_package
+cd /tensorflow
+echo '\n\n\nn\nn\nn\n\n\n\n\n\n\n\n\n\n\n\n' | ./configure
+bazel build --config=monolithic --config=opt --config=cuda //tensorflow:libtensorflow_cc.so
+bazel build --config=opt --config=cuda //tensorflow/tools/pip_package:build_pip_package
 mkdir -p /tmp/tensorflow_pkg
 bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
 pip install /tmp/tensorflow_pkg/tensorflow-1.5.1-cp27-cp27mu-linux_x86_64.whl
@@ -23,7 +12,7 @@ cp -r tensorflow/cc /usr/local/include/tensorflow/
 cp bazel-genfiles/tensorflow/cc/ops/*.h /usr/local/include/tensorflow/cc/ops/
 cp bazel-bin/tensorflow/libtensorflow_cc.so /usr/local/lib/
 
-cd /vcs
-rm -rf /tmp/tensorflow_pkg
-rm -rf ./tensorflow
+#cd /vcs
+#rm -rf /tmp/tensorflow_pkg
+#rm -rf /tensorflow
 
