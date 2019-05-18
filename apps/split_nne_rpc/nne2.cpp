6e68d32e40bc6bf614fbb9f20dc8f86e626c39ff
Chameleon Cloud User <cc@gpu6.novalocal>
2019-05-18 18:00:27 +0000

    update nne2

--- apps/split_nne_rpc/nne2.cpp
+++ apps/split_nne_rpc/nne2.cpp
@@ -11,7 +11,6 @@
 #include "common/context.h"
 #include "model/model_manager.h"
 #include "processor/neural_net_evaluator.h"
-#include "processor/image_classifier.h"
 #include "processor/rpc/frame_receiver.h"
 #include "processor/processor.h"
 
@@ -32,16 +31,11 @@ void Run(const std::string& subscribe_endpoint,
   auto nne = std::make_shared<NeuralNetEvaluator>(model_desc, input_shape, 1, output_layers);
   nne->SetSource(receiver->GetSink(), input_layer);
 
-  //classifier (NNC)
-  auto classifier = std::make_shared<ImageClassifier>(model_desc, 1);
-  classifier->SetSource("input", nne->GetSink());
-
-  auto reader = classifier->GetSink("output")->Subscribe();
+  auto reader = nne->GetSink()->Subscribe();
   int frame_count = 0;
-  
+  int frame_id = 0;
   auto processing_start_micros_ = boost::posix_time::microsec_clock::local_time();
 
-  classifier->Start();
   nne->Start();
   receiver->Start();
   //move it to a function?
@@ -50,23 +44,20 @@ void Run(const std::string& subscribe_endpoint,
     auto frame = reader->PopFrame(20);
     if(frame != NULL) {
         frame_count ++;
+	frame_id = frame->GetValue<unsigned long>("frame_id");
     }
     auto passing_time = boost::posix_time::microsec_clock::local_time() - processing_start_micros_;
     if (passing_time.total_seconds() > exec_sec){
-        auto last_id = frame->GetValue<unsigned long>("frame_id");
-        auto drop_rate = (float) (last_id - frame_count) / last_id;
+        auto drop_rate = (float) (frame_id - frame_count) / frame_id;
         std::cout << "======" << std::endl;
         std::cout << "frame count = " << frame_count << std::endl;
-        std::cout << "last id = " << last_id << std::endl;
+        std::cout << "last id = " << frame_id << std::endl;
         std::cout << "drop rate = " << drop_rate << std::endl;
         std::cout << "nne fps = " << nne->GetHistoricalProcessFps() << std::endl;
         std::cout << "nne latency = " << nne->GetAvgProcessingLatencyMs() << std::endl;
         std::cout << "nne queue = " << nne->GetAvgQueueLatencyMs() << std::endl;
         std::cout << "data size = " << receiver->GetMsgByte() << std::endl;
         std::cout << "deserialize latency = " << receiver->GetTotalDeserialLatencyMs() / frame_count << std::endl;
-        std::cout << "classifier fps = " << classifier->GetHistoricalProcessFps() << std::endl;
-        std::cout << "classifier latency = " << classifier->GetAvgProcessingLatencyMs() << std::endl;
-        std::cout << "classifier queue = " << classifier->GetAvgQueueLatencyMs() << std::endl;
         break;
     }
     
@@ -75,7 +66,6 @@ void Run(const std::string& subscribe_endpoint,
   // Stop the processors in forward order.
   receiver->Stop();
   nne->Stop();
-  classifier->Stop();
 }
 
 int main(int argc, char* argv[]) {
